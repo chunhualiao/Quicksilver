1e3d713dfc1449bf3d30179ba1ee9bc7b0dbe21b
Chunhua Liao <liao6@lnl.gov>
2022-01-19 14:07:53 -0800

    more makefiles for serial, etc

--- src/run.sh
+++ src/run.sh
@@ -4,4 +4,9 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/tce/packages/cuda/cuda-10.1.243/lib
 export OMP_NUM_THREADS=1
 # gpu version
 #./qs.gpu -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.gpu.p1_1.output.txt
-./qs.cpu.omp -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.cpu.omp.1thread.p1_1.output.txt
+
+# OMP version
+#./qs.cpu.omp -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.cpu.omp.1thread.p1_1.output.txt
+
+# serial version
+./qs.serial -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.serial.p1_1.output.txt
