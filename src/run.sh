ceb37c9262cb325af71f12d05881702ac8511def
Chunhua Liao <liao6@lnl.gov>
2022-01-19 13:57:38 -0800

    cpu OpenMP version on lassen

--- src/run.sh
+++ src/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/tce/packages/cuda/cuda-10.1.243/lib64
 
-./qs.gpu -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.gpu.p1_1.output.txt
+export OMP_NUM_THREADS=1
+# gpu version
+#./qs.gpu -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.gpu.p1_1.output.txt
+./qs.cpu.omp -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.cpu.omp.1thread.p1_1.output.txt
