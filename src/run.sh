fbf89fd378b5e9bfa4d183b0d40299c06fb8bb3e
Chunhua Liao <liao6@lnl.gov>
2022-01-26 16:28:24 -0800

    fixed some linking errors: two functions needed omp declare target directives.

--- src/run.sh
+++ src/run.sh
@@ -2,8 +2,8 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/tce/packages/cuda/cuda-10.1.243/lib64
 
 #export OMP_NUM_THREADS=1
-# gpu version, with gpu disabled
-./qs.gpu -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.gpu.disabled.p1_1.output.txt
+# gpu version, with gpu enabled
+./qs.gpu -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.gpu.clang-gpu.lassen.p1_1.output.txt
 
 # OMP version
 #./qs.cpu.omp -i ../Examples/CORAL2_Benchmark/Problem1/Coral2_P1_1.inp &> qs.cpu.omp.1thread.p1_1.output.txt
